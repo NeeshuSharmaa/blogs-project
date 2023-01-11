0af4ee30692c586365ab874de75139704f9b9bd5
NeeshuSharmaa <neeshusharmaa@gmail.com>
2023-01-11 16:27:30 +0530

    Implemented: useEffect

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Blogs from "./components/Blogs";
 
 function App() {
-  var [blogs, setBlogs] = useState([
-    {
-      title: "The Existential Crisis",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat....",
-      publishedDate: "October 13, 2014",
-      id: "1",
-    },
-    {
-      title: "The Paradigm of Decisions",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat....",
-      publishedDate: "December 13, 2014",
-      id: "2",
-    },
-    {
-      title: "Sloth ready to Grind",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat....",
-      publishedDate: "January 13, 2014",
-      id: "3",
-    },
-  ]);
-  function deleteHandler(id) {
-    var newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  var [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Search blogList={blogs} setBlogs={setBlogs} />
-      <Blogs blogs={blogs} deleteHandler={deleteHandler} />
+      <Blogs blogs={blogs} />
     </div>
   );
 }
