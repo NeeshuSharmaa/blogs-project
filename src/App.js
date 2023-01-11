4fd6910a9f00aa8583b7581ceb4aaf0b2254edbe
NeeshuSharmaa <neeshusharmaa@gmail.com>
2023-01-11 16:35:35 +0530

    Feat: conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import Blogs from "./components/Blogs";
 
 function App() {
   var [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => res.json())
-      .then((data) => setBlogs(data));
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+      });
   }, []);
 
   return (
     <div className="App">
       <Header />
       <Search blogList={blogs} setBlogs={setBlogs} />
-      <Blogs blogs={blogs} />
+      <Blogs blogs={blogs} isPending={isPending} />
     </div>
   );
 }
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Blog from "./Blog";
 
-export default function Blogs({ blogs, deleteHandler }) {
+export default function Blogs({ blogs, deleteHandler, isPending }) {
   return (
     <div className="blogsSection">
       <div className="blogs-div">
-        {blogs.map((blog) => (
-          <Blog blog={blog} key={blog.id} deleteHandler={deleteHandler} />
-        ))}
+        {isPending && <div>Loading....</div>}
+        {blogs &&
+          blogs.map((blog) => (
+            <Blog blog={blog} key={blog.id} deleteHandler={deleteHandler} />
+          ))}
       </div>
     </div>
   );
