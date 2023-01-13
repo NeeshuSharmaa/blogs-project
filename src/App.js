249f0e370899c7f22ae80a9e100841c2ec2c1731
NeeshuSharmaa <neeshusharmaa@gmail.com>
2023-01-13 20:37:53 +0530

    React Router update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "./components/Header";
-import Search from "./components/Search";
-import Blogs from "./components/Blogs";
 import useFetch from "./useFetch";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import BlogDetails from "./components/BlogDetails";
 import Create from "./components/Create";
+import Home from "./components/Home";
 
 function App() {
-  const {
-    data: blogs,
-    setData: setBlogs,
-    isPending,
-    error,
-  } = useFetch("http://localhost:8000/blogs");
-  console.log("rendering", blogs, isPending, error);
   return (
     <Router>
       <div className="App">
         <Header />
-        <Switch>
-          <Route exact path="/">
-            <Search blogList={blogs} setBlogs={setBlogs} />
-            <Blogs blogs={blogs} isPending={isPending} error={error} />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/blogs/:id">
-            <BlogDetails />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+        </Routes>
       </div>
     </Router>
   );
