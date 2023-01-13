9d06198a68d78db4dbe9b7bd42c0a4a7e6700fe6
NeeshuSharmaa <neeshusharmaa@gmail.com>
2023-01-13 20:40:44 +0530

    feat: delete-btn

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,13 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "../useFetch";
+import { useState } from "react";
 
 const BlogDetails = () => {
+  const navigate = useNavigate();
+
   const { id } = useParams();
   const {
     data: blog,
     isPending,
     error,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  function deleteHandler(event) {
+    event.preventDefault();
+
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then((res) => console.log(`blog with id: ${blog.id} got deleted`));
+    navigate("/");
+  }
   return (
     <div className="blog-details">
       {isPending && <div>Loading.....</div>}
@@ -17,6 +29,9 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>{blog.body}</p>
           <small>{blog.publishedDate}</small>
+          <button className="delete-btn" onClick={deleteHandler}>
+            Delete
+          </button>
         </article>
       )}
     </div>
